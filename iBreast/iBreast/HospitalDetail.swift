63cb3c81cd26d85ab28b457b11c392aa06a23722
xushiyong <835588741@qq.com>
2015-08-02 17:31:10 +0800

    医院详情页面

---
+++ iBreast/iBreast/HospitalDetail.swift
@@ -0,0 +1,23 @@
+//
+//  HospitalDetail.swift
+//  iBreast
+//
+//  Created by 许仕永 on 15/8/2.
+//  Copyright (c) 2015年 钟其鸿. All rights reserved.
+//
+import UIKit
+
+class HospitalDetail: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
