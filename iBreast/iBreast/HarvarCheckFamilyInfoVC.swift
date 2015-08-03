ddc0f0a32b71f15f5bcfece72d701ec6d5943a3a
martian <441699771@qq.com>
2015-08-04 01:14:52 +0800

    修改家族癌症历史添加bug

--- iBreast/iBreast/HarvarCheckFamilyInfoVC.swift
+++ iBreast/iBreast/HarvarCheckFamilyInfoVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 var mHarvardCheckModel:HarvardCheckModel!
 
+class SectionRow{
+    var section:Int = -1
+    var row:Int = -1
+    
+    //0:表示选择的是血缘关系 1：选择的是癌症年龄和癌症名字,-Int.max:表示该选择无效
+    var tag:Int = -1
+}
+
 class HarvarCheckFamilyInfoVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
@@ -18,6 +26,9 @@ class HarvarCheckFamilyInfoVC: UIViewController {
     var tag:Int = 0
     var age:[Int]=[Int]()
     let pickerAlert:AlertPickerViewController=AlertPickerViewController()
+    var sectionRow = SectionRow()
+    var bloodline:String!
+    var relationship:String!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -81,20 +92,36 @@ extension HarvarCheckFamilyInfoVC:UITableViewDataSource,UITableViewDelegate{
         if(indexPath.section<familyInfos?.count)
         {
             if(indexPath.row==familyInfos![indexPath.section]._cancerCount-1)
-            {
+            {//增加一行癌症历史
                 println("add new item in the section")
                 tag = 1
                 sectionId = indexPath.section;
-                pickerAlert.showPickerInActionSheet(indexPath.row)
+                
+                var newItem=familyInfos![indexPath.section]
+                newItem.addCancer("第\(familyInfos![indexPath.section]._cancerCount)次癌症", age:20)
+                
+
+                 tableView.reloadData()
+                
+                 resetSectionRow()
+                //pickerAlert.showPickerInActionSheet(indexPath.row)
             }
             else if(indexPath.row == 0){//选择血缘关系
                  tag = 0
+                
+               
+                 sectionRow.tag = 0
+                
                  pickerAlert.showPickerInActionSheet(indexPath.row)
             }
             else {//选择癌症历史
                 tag = 1
+                sectionRow.tag = 1
                 pickerAlert.showPickerInActionSheet(indexPath.row)
+                
             }
+            sectionRow.row = indexPath.row
+            sectionRow.section = indexPath.section
         }
         // 点击了固定section之外的增加
         else{
@@ -105,11 +132,17 @@ extension HarvarCheckFamilyInfoVC:UITableViewDataSource,UITableViewDelegate{
             //family.addCancer("乳腺癌", age:18)
             
 
-            
+            resetSectionRow()
             
             tableView.reloadData()
 
         }
+    }
+    
+    func resetSectionRow(){
+        sectionRow.row = -Int.max
+        sectionRow.section = -Int.max
+        sectionRow.tag = -Int.max
     }
     
      func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat{
@@ -195,11 +228,39 @@ extension HarvarCheckFamilyInfoVC:AlertPickerViewControllerDelegate{
     func didSelect(){
         println("didSelect")
         
-        var newItem=familyInfos![sectionId]
-        newItem.addCancer(cancer,age:ageTemp)
+//        var newItem=familyInfos![sectionId]
+//        newItem.addCancer(cancer,age:ageTemp)
         //
-        tableView.reloadData()
+        //tableView.reloadData()
         //tableBaseInof2.reloadData();
+        
+        if(sectionRow.tag == -Int.max){
+            return
+        }
+        else{
+            
+            if(sectionRow.tag == 0){
+                //选择的是血缘关系
+//                
+                var newItem=familyInfos![sectionRow.section]
+//                
+                newItem._relationship = relationship
+                newItem._bloodline = bloodline
+                
+            }
+            else if(sectionRow.tag == 1){
+                //选择的是癌症历史
+                
+                var newItem=familyInfos![sectionRow.section]
+                
+                newItem._age[sectionRow.row] = ageTemp
+                newItem._cancerName[sectionRow.row] = cancer
+                
+                
+              //  newItem.addCancer("第\(familyInfos![indexPath.section]._cancerCount)次癌症", age:20)
+            }
+            tableView.reloadData()
+        }
     }
     func didCancel()
     {
@@ -265,11 +326,23 @@ extension HarvarCheckFamilyInfoVC:UIPickerViewDataSource ,UIPickerViewDelegate{
         
         if(component == 0)
         {
-            cancer = "\(mHarvardCheckModel.cancersForSelf[row])"
+            if(tag == 1){
+                cancer = "\(mHarvardCheckModel.cancersForSelf[row])"
+            }
+            else{
+               relationship = mHarvardCheckModel.relationShips[row];
+            }
+            
         }
         else
         {
-            ageTemp = mHarvardCheckModel.age[row]
+            if(tag == 1){
+                   ageTemp = mHarvardCheckModel.age[row]
+            }
+            else{
+                bloodline = "\(mHarvardCheckModel.bloodLines[row])"
+            }
+         
         }
     }
     
